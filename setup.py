57ffa71f05e37b6b3d6524f110a07a8902d6d3cc
ArunKhare <100008999+ArunKhare@users.noreply.github.com>
2022-07-27 16:56:57 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -8,13 +8,19 @@ AUTHOR = "ARUN KHARE"
 DESCRIPTION = "Automate the investment predictions process"
 REQUIREMENTS_FILE_NAME= "requirements.txt"
 
+HYPEN_E_DOT = "-e ."
+
 def get_requirements_list()->List[str]:
     ''' 
     Description : This function is going to return list mention in requirements.txt file
     return: This funtion is going to return a list which contain list of libraries mentioned in requirements.txt file
     '''
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove('-e .')   # .pop('-e .') / .remove('-e .')
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+        if HYPEN_E_DOT in requirement_file:
+            requirement_list.remove(HYPEN_E_DOT)
+        return requirement_list
 
 setup(
     name=PROJECT_NAME,
